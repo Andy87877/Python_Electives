7d4181c5f68eb5c6391c9c2ebfe547f2872a5b64
Andy8787(布長) <andy8787main@gmail.com>
2022-04-06 14:11:20 +0800

    無set版

--- 串列中去除重複的數字(無set版).py
+++ 串列中去除重複的數字(無set版).py
@@ -2,12 +2,7 @@ l = [1,2,3,4,2,7,3,2,3]
 ansl = []
 sorted(l)
 for i in l:
-    flag = True
-    for j in ansl:
-        if (i == j):
-            flag = False
-            break
-    if flag:
+    if i not in ansl:
         ansl.append(i)
 print(ansl)
 # print(sorted(list(set(l))))
