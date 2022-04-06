b9018c1ffb08379d10bae68e124657088337623a
Andy8787(布長) <andy8787main@gmail.com>
2022-04-06 15:17:25 +0800

    not in PM

--- PM2.5.py
+++ PM2.5.py
@@ -6,4 +6,7 @@ PM["台南市"] = 3
 PM["桃園市"] = 5
 PM["高雄市"] = 9
 city = input("輸入要查詢的六都名稱:")
-print(city,"今天的PM2.5值為:",PM[city])
+if city not in PM:
+    print("六都中沒有[%s]城市!"%(city))
+else:
+    print(city,"今天的PM2.5值為:",PM[city])
